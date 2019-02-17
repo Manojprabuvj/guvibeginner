fd1a282e5ebde602dd94933a4371eb28c1fb8635
subash789 <46813541+subash789@users.noreply.github.com>
2019-02-17 21:41:35 +0530

    Create set1-2.c

---
+++ set1-2.c
@@ -0,0 +1,21 @@
+#include<stdio.h> 
+ #include<conio.h> 
+ int main() 
+ { 
+ int n; 
+ printf("enter the number:"); 
+ scanf("%d",&n); 
+ if(n%2==0) 
+{ 
+   printf("%d is even",n); 
+} 
+ else if(n<0) 
+ { 
+   printf("invalid input"); 
+ }   
+ else 
+ { 
+   printf("%d is odd",n); 
+ } 
+ return 0; 
+ } 
