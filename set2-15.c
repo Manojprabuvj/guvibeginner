e1dcb234f29ebd8efaf5e4b53a01a9cbfb531b51
subash789 <46813541+subash789@users.noreply.github.com>
2019-02-21 00:29:30 +0530

    Create set2-15.c

---
+++ set2-15.c
@@ -0,0 +1,16 @@
+#include <stdio.h>  
+    int main()
+    {
+      int n,k,i;
+	    scanf("%d",&n);
+      scanf("%d",&k);
+      for(i=n+1;i<k;i++)
+      {
+         if(i%2==0)
+         {
+           printf("%d ",i);
+         }
+      }
+      return 0;
+    }
+
