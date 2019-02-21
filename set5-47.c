89f07e8e5cb1c8669f174ac02fa75d223e5c687a
subash789 <46813541+subash789@users.noreply.github.com>
2019-02-22 00:50:57 +0530

    Create set5-47.c

---
+++ set5-47.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+   #include<math.h>  
+    int main()
+    {
+        int i, j,n,s,a[30];
+        scanf("%d",&n);
+        for(i=0;i<n;i++)
+        {
+            scanf("%d",&a[i]);
+        }
+        for(i=0;i<n;i++) 
+        {
+            for(j=i+1;j<n;j++)
+            {
+                if(a[i]>a[j]) 
+                {
+                    s=a[i];
+                    a[i]=a[j];
+                    a[j]=s;
+                }
+            }
+        }
+        for (i = 0; i < n; ++i)
+        {
+            printf("%d ",a[i]);
+            printf("%d",a[n-1]);
+            break;
+        }
+      return 0;
+    }
