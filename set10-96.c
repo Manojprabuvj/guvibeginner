747d85f5bd6af172c8dede66c134d21cdfc69d11
subash789 <46813541+subash789@users.noreply.github.com>
2019-02-25 01:01:03 +0530

    Create set10-96.c

---
+++ set10-96.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(void) 
+{
+ int a,b,c,z,s,i;
+ scanf("%d\t%d\t%d",&a,&b,&c);
+ s=(c*(2*a+(c-1)*b))/2;
+  z=a+(c-1)*b;
+  for(i=a;i<=z;i++)
+  {
+    if(i==z)
+    {
+      printf("%d",s);
+    }
+  }
+ 
+	return 0;
+}
